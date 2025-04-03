06b82bfbe10c57d44b725e0c6c0bcfe3b69632d9
Riz <55377396+Riz-0@users.noreply.github.com>
2025-04-03 19:15:39 +0800

    Simplify operations

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const operands = [];
 const equation = document.querySelector("h2");
 const total = document.querySelector("h1");
+let lastOperation = "";
 
 function main() {
   const numberBtns = document.querySelectorAll(".number");
   numberBtns.forEach((btn) => btn.addEventListener("click", numPress));
 
   const addBtn = document.querySelector(".add");
-  addBtn.addEventListener("click", add);
+  addBtn.addEventListener("click", operate);
+  const subBtn = document.querySelector(".sub");
+  subBtn.addEventListener("click", operate);
+  const multiplyBtn = document.querySelector(".mul");
+  multiplyBtn.addEventListener("click", operate);
+  const divideBtn = document.querySelector(".div");
+  divideBtn.addEventListener("click", operate);
+  const modBtn = document.querySelector(".mod");
+  modBtn.addEventListener("click", operate);
 }
 
 function numPress(e) {
@@ -15,13 +24,30 @@ function numPress(e) {
   equation.textContent += e.target.textContent;
 }
 
-function add() {
+function operate(e) {
   getNewNumber();
   if (operands.length > 1) {
-    operands[0] = operands[0] + operands[1];
+    switch (lastOperation) {
+      case "+":
+        operands[0] = operands[0] + operands[1];
+        break;
+      case "-":
+        operands[0] = operands[0] - operands[1];
+        break;
+      case "*":
+        operands[0] = operands[0] * operands[1];
+        break;
+      case "/":
+        operands[0] = operands[0] / operands[1];
+        break;
+      case "%":
+        operands[0] = operands[0] % operands[1];
+        break;
+    }
     operands.pop();
   }
-  equation.textContent = `${operands[0]} + `;
+  lastOperation = e.target.textContent;
+  equation.textContent = `${operands[0]} ${lastOperation} `;
   total.textContent = `${operands[0]}`;
 }
 
