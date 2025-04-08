591a0f488d869f4fbd76ba32d1452d6e3ee445e9
Riz <55377396+Riz-0@users.noreply.github.com>
2025-04-08 23:42:27 +0800

    Add keyboard functionality for numbers

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ function main() {
   numBtns.forEach((btn) =>
     btn.addEventListener("click", (e) => numPress(e.target.textContent))
   );
+
+  // Keyboard Support
+  document.addEventListener("keydown", (e) => keyHandler(e.key));
 }
 
 function numPress(num) {
+  // Did the user just press a sign / Is it the first calculation?
   if (calcDone) total.textContent = "";
   total.textContent += num;
   calcDone = false;
+}
+
+function keyHandler(key) {
+  const numbers = "0123456789";
+  if (numbers.includes(key)) numPress(key);
 }
 
 main();
