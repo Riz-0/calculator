5d0cfcaf35bf066a8ba1afb744b776bf7d25774d
Riz <55377396+Riz-0@users.noreply.github.com>
2025-04-09 14:52:35 +0800

    Add sign functionality

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const total = document.querySelector("h1");
+const equation = document.querySelector("h2");
 let calcDone = true;
 
 function main() {
+  // Num Buttons
   const numBtns = document.querySelectorAll(".number");
   numBtns.forEach((btn) =>
     btn.addEventListener("click", (e) => numPress(e.target.textContent))
+  );
+
+  // Sign Buttons
+  const signBtns = document.querySelectorAll(".sign");
+  signBtns.forEach((btn) =>
+    btn.addEventListener("click", (e) => signPress(e.target.textContent))
   );
 
   // Keyboard Support
@@ -13,9 +20,34 @@ function main() {
 
 function numPress(num) {
   // Did the user just press a sign / Is it the first calculation?
-  if (calcDone) total.textContent = "";
-  total.textContent += num;
+  if (equation.textContent === "UwU") equation.textContent = "";
+  equation.textContent += num;
   calcDone = false;
+}
+
+function signPress(sign) {
+  const total = document.querySelector("h1");
+  const prevSign = equation.textContent.split(" ")[1];
+  // Is user continuing from a previous calculation?
+  let result =
+    equation.textContent === "UwU"
+      ? parseFloat(total.textContent)
+      : operate(prevSign);
+  equation.textContent = `${result} ${sign} `;
+  total.textContent = result;
+  calcDone = true;
+}
+
+function operate(sign) {
+  const operands = equation.textContent.split(" ");
+  // Did user enter anything after the sign?
+  if (operands[2] === undefined || operands[2] === "") return operands[0];
+  switch (sign) {
+    case "+":
+      return parseFloat(operands[0]) + parseFloat(operands[2]);
+    case "-":
+      return parseFloat(operands[0]) - parseFloat(operands[2]);
+  }
 }
 
 function keyHandler(key) {
